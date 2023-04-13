9d94584ef3fbc691c4c7d5c73b8cafc372289e23
d.sadybekov <67111046+d-sadybekov@users.noreply.github.com>
2023-04-13 12:52:44 +0500

    Рефакторинг
    Замена объектов массивами. Применены новые методы

--- cypress/e2e/HW/hw-param.cy.js
+++ cypress/e2e/HW/hw-param.cy.js
@@ -10,31 +10,28 @@ describe("Authorization page tests", () => {
   //1.2 Positive - restore password
   it("1.2 Positive - restore password", () => {
     onePage.buttonIdentAndClick("btn","Забыли пароль?", 1)
-    onePage.chekContent("page", "Восстановите пароль")
+    onePage.checkMessage("Восстановите пароль")
     onePage.typeText("d.sadybekov@yandex.ru", 2)
-    cy.get("#exitRestoreButton").should("be.visible")
+    onePage.checkElementVisible("exitRestoreBtn")
     onePage.buttonIdentAndClick("btn", "Отправить код", 2)
-    onePage.chekContent("page", "Успешно отправили пароль на e-mail")
-    cy.get("#exitMessageButton").should("be.visible")
-  })
+    onePage.checkMessage("Успешно отправили пароль на e-mail")
+    onePage.checkElementVisible("exitMessageBtn")
+  });
   
   //1.1, 1.3, 1.4, 1.5, 1.6
-    const ElementsArray=[
-        ["1.1 Positive - correct login(mail)/password",[{itm:"german@dolnikov.ru",indx:0},{itm:"iLoveqastudio1",indx:1}],"btn","Войти", "page", "Авторизация прошла успешно"],
-        ["1.3 Negative - correct login(mail), incorrect password",[{itm:"german@dolnikov.ru", indx:0},{itm:"iLoveqastudio!",indx:1}],"btn", "Войти", "page", "Такого логина или пароля нет"],
-        ["1.4 Negative - incorrect login(mail), correct password",[{itm:"german@do1nikov.ru", indx:0},{itm:"iLoveqastudio1",indx:1}],"btn", "Войти", "page", "Такого логина или пароля нет"],
-        ["1.5 Negative - incorrect login(mail), correct password",[{itm:"germandolnikov.ru", indx:0},{itm:"iLoveqastudio1",indx:1}],"btn", "Войти", "page", "Нужно исправить проблему валидации"],
-        ["1.6 Positive - login to lower case - correct login(mail)/password",[{itm:"GerMan@Dolnikov.ru",indx:0},{itm:"iLoveqastudio1",indx: 1}],"btn", "Войти", "page", "Авторизация прошла успешно"]
-    ]
-    ElementsArray.forEach(Element => {
-    it(Element[0], () => {
-        const innerElementsArray =Element[1]
-        innerElementsArray.forEach((innerElement) => {
-        onePage.typeText(innerElement.itm, innerElement.indx)
+   [
+        ["1.1 Positive - correct login(mail)/password",[["german@dolnikov.ru", 0],["iLoveqastudio1", 1]],"btn","Войти", "Авторизация прошла успешно"],
+        ["1.3 Negative - correct login(mail), incorrect password",[["german@dolnikov.ru", 0],["iLoveqastudio!", 1]],"btn", "Войти", "Такого логина или пароля нет"],
+        ["1.4 Negative - incorrect login(mail), correct password",[["german@do1nikov.ru", 0],["iLoveqastudio1", 1]],"btn", "Войти", "Такого логина или пароля нет"],
+        ["1.5 Negative - incorrect login(mail), correct password",[["germandolnikov.ru", 0],["iLoveqastudio1", 1]],"btn", "Войти", "Нужно исправить проблему валидации"],
+        ["1.6 Positive - login to lower case - correct login(mail)/password",[["GerMan@Dolnikov.ru", 0],["iLoveqastudio1", 1]],"btn", "Войти", "Авторизация прошла успешно"]
+    ].forEach(Element => {   it(Element[0], () => {
+        Element[1].forEach((innerElement) => {
+        onePage.typeText(innerElement[0], innerElement[1])
       })
       onePage.buttonIdentAndClick(Element[2], Element[3], 0)
-      onePage.chekContent(Element[4], Element[5], 0)
-      cy.get("#exitMessageButton").should("be.visible")
+      onePage.checkMessage(Element[4])
+      onePage.checkElementVisible("exitMessageBtn")
     })
   })
 })
